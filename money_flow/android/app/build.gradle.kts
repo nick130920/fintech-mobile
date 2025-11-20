e291b3508540cdb523dde19f404068ff5ee3be6f
Nicolas Andres Muñoz <namc1309@gmail.com>
2025-11-20 18:58:22 -0500

    Enable core library desugaring in build configuration
    - Added support for core library desugaring in the Android build.gradle.kts file to allow usage of newer Java language features on older Android versions.
    - Included the necessary dependency for desugaring to ensure compatibility with Java 8+ APIs.

--- money_flow/android/app/build.gradle.kts
+++ money_flow/android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -56,4 +57,5 @@ flutter {
 
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
